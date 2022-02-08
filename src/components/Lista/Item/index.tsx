59a8f1e398e96df00efae8cddb13c680e8cf70fc
Esther Marie <24815585+EstherMarie@users.noreply.github.com>
2022-02-08 16:18:17 -0300

    Aula 5
    O que foi feito:
    - Estados adicionados em function components e class components
    - Inputs do Form com estado
    - Pasta types criada para reutilizar Interfaces do TypeScript
    - Adicionado atributo lang no HTML e title atualizado

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -1,6 +1,8 @@
+import { ITarefa } from '../../../types/tarefa';
 import style from '../Lista.module.scss';
 
-export default function Item({ tarefa, tempo }: { tarefa: string, tempo: string}) {
+export default function Item({ tarefa, tempo, selecionado, completo, id }: ITarefa) {
+  console.log('Item supimpa: ',{ tarefa, tempo, selecionado, completo, id })
   return (
     <li className={style.item}>
       <h3>{tarefa}</h3>
--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,31 +1,8 @@
-import React from "react";
 import style from "./Lista.module.scss";
 import Item from "./Item";
+import { ITarefa } from "../../types/tarefa";
 
-interface Tarefa {
-  tarefa: string;
-  tempo: string;
-}
-
-export default function Lista() {
-  const tarefas: Array<Tarefa> = [
-    {
-      tarefa: "React",
-      tempo: "02:00:00",
-    },
-    {
-      tarefa: "JavaScript",
-      tempo: "01:30:00",
-    },
-    {
-      tarefa: "TypeScript",
-      tempo: "03:00:00",
-    },
-    {
-      tarefa: "Sass",
-      tempo: "03:30:00",
-    },
-  ];
+export default function Lista({ tarefas }: { tarefas: ITarefa[]}) {
   return (
     <aside className={style.listaTarefas}>
       <h2>Estudos do dia</h2>
