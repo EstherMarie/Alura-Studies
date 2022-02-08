f3cc3a228240373872154dbcc2270b209f415083
Esther Marie <24815585+EstherMarie@users.noreply.github.com>
2022-02-08 16:18:17 -0300

    Aula 5 - Em andamento
    Falta terminar: Aula 5.4 - Atualizando a tarefa
    
    O que foi feito:
    - Estados adicionados em function components e class components
    - Inputs do Form com estado
    - Pasta types criada para reutilizar Interfaces do TypeScript
    - Adicionado atributo lang no HTML e title atualizado

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Cronometro from "../components/Cronometro";
 import Formulario from "../components/Formulario";
 import Lista from "../components/Lista";
+import { ITarefa } from "../types/tarefa";
 import style from "./App.module.scss";
 
 function App() {
+  const [tarefas, setTarefas] = useState<ITarefa[]>([])
+
   return (
     <main className={style.AppStyle}>
       <h1>Alura Studies</h1>
-      <Formulario />
+      <Formulario setTarefas={setTarefas} />
       <Cronometro />
-      <Lista />
+      <Lista tarefas={tarefas} />
     </main>
   );
 }
