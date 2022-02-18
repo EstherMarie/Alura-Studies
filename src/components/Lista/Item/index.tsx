97e0bec91160a74c0352d44dbcc5eb6870ccfe80
Esther Marie <24815585+EstherMarie@users.noreply.github.com>
2022-02-18 16:01:40 -0300

    Aula 7 - Aplicação completa
    Falta terminar: Aula 7.5 - Refatorando Class Components
    
    O que foi feito:
    - Utilização do useEffect
    - Uso de função recursiva
    - Uso de renderização condicional

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -15,10 +15,10 @@ export default function Item({
 }: Props) {
   return (
     <li
-      className={`${style.item} ${selecionado ? style.itemSelecionado : ''}`}
+      className={`${style.item} ${selecionado ? style.itemSelecionado : ''} ${completo ? style.itemCompletado : ''}`}
       tabIndex={0}
       onClick={() => {
-        selecionaTarefa({
+        !completo && selecionaTarefa({
           tarefa,
           tempo,
           selecionado,
@@ -38,6 +38,8 @@ export default function Item({
     >
       <h3>{tarefa}</h3>
       <span>{tempo}</span>
+      {/* Renderização condicional */}
+      {completo && <span className={style.concluido} aria-label="tarefa concluída"></span>}
     </li>
   );
 }
